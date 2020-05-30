72ae75fcbdab86bc71f4e690ff0dcfa3c056271e
ALEX\alex <lyssenko.alex@gmail.com>
2020-05-31 02:53:00 +0600

    feat: build

---
+++ fancy-weather/build/precache-manifest.3c7d25c58d02c0eea45c187f4326364a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ea70dd295ab66e57505a27d0d4d3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e6d3755b1e519d21d3",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "7556edeae954deec4d39",
+    "url": "/static/css/main.c1993faf.chunk.css"
+  },
+  {
+    "revision": "84e6d3755b1e519d21d3",
+    "url": "/static/js/2.30f037e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.30f037e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7556edeae954deec4d39",
+    "url": "/static/js/main.0c4f41ed.chunk.js"
+  },
+  {
+    "revision": "7b84cece31479a82f2d3",
+    "url": "/static/js/runtime-main.16b261c7.js"
+  },
+  {
+    "revision": "2663d148d311938a815d12c42beb09f1",
+    "url": "/static/media/Weather_04.2663d148.jpg"
+  }
+]);
